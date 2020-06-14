9f2b616908e2015796ca0268a49d131b9a6e8ffe
Bharat Nagda <nagdabharat@gmail.com>
2020-06-14 15:08:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,9 +67,9 @@ function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber >= 0) {
     return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    cart.length = 0;
     //cart = [{}];
   } else {
     return("Sorry, we don't have a credit card on file for you.")
   }
+  cart.length = 0;
 }
