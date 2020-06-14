bdd19cc65229eb591c902064214200a6b81fb52b
Bharat Nagda <nagdabharat@gmail.com>
2020-06-14 15:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ function placeOrder(cardNumber) {
   } else {
     return("Sorry, we don't have a credit card on file for you.")
   }
-  var nullArray = [];
+  var nullArray = [{}];
   cart = nullArray;
 }
